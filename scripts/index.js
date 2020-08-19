4ec859493550ef8cf26a6574c1984bbf8d339a8b
Leonid Nenkin <nenkin.leonid@gmail.com>
2020-08-19 13:17:08 +0300

    Draft 1908_v1

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+// Находим форму в DOM
+let formElement = document.querySelector('.popup-form');
+let editButton = document.querySelector('.profile__btn_action_edit');
+let formCloseButton = document.querySelector('.close-icon');
+
+    // Обработчик «отправки» формы, хотя пока
+    // она никуда отправляться не будет
+    function formSubmitHandler(evt) {
+        evt.preventDefault();
+        // Эта строчка отменяет стандартную отправку формы.
+        // Так мы можем определить свою логику отправки.
+        // О том, как это делать, расскажем позже.
+
+        // Находим поля формы в DOM
+        let nameInput = formElement.querySelector('.popup-form__text_type_name');
+        let jobInput = formElement.querySelector('.popup-form__text_type_description');
+
+    // Получите значение полей из свойства value
+        let name = nameInput.value;
+        let job = jobInput.value;
+    // Выберите элементы, куда должны быть вставлены значения полей
+        let ProfileName = document.querySelector('.profile__name');
+        let ProfileDesc = document.querySelector('.profile__description');
+    // Вставьте новые значения с помощью textContent
+        ProfileName.textContent = name;
+        ProfileDesc.textContent = job;
+}
+
+function openCloseFormHandler(evt) {
+    evt.preventDefault();
+    let editForm = document.querySelector('.popup');
+    
+    editForm.classList.toggle('popup_opened');
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openCloseFormHandler);
+formCloseButton.addEventListener('click', openCloseFormHandler);
