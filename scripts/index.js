d9b9e08d9aace1a5f61f064a5750036b4a48cfa7
Leonid Nenkin <nenkin.leonid@gmail.com>
2020-08-29 14:10:56 -0700

    Draft 29.08

--- scripts/index.js
+++ scripts/index.js
@@ -1,112 +1,65 @@
-let editButton = document.querySelector('.profile__btn_action_edit');
-let addButton = document.querySelector('.profile__btn_action_add');
+const editButton = document.querySelector('.profile__btn_action_edit');
+const addButton = document.querySelector('.profile__btn_action_add');
 
-let ProfileName = document.querySelector('.profile__name');
-let ProfileDesc = document.querySelector('.profile__description');
+const profileName = document.querySelector('.profile__name');
+const profileDesc = document.querySelector('.profile__description');
 
-let nameInput;
-let jobInput;
-let formSaveButton;
-let formCloseButton;
+// edit form popup
+const popupEditForm = document.querySelector('.page__popup_edit-form');
+const nameInputEditForm = popupEditForm.querySelector('.popup__text-field_type_name');
+const jobInputEditForm = popupEditForm.querySelector('.popup__text-field_type_description');
+const saveButtonEditForm = popupEditForm.querySelector('.popup__btn_action_save');
+const closeButtonEditForm = popupEditForm.querySelector('.popup__btn_action_close');
+// edit form events
+popupEditForm.addEventListener('submit', formSubmitHandler);
+closeButtonEditForm.addEventListener('click', function() {togglePopupHandler(popupEditForm)});
 
-const cardsListTemplate = document.querySelector("#cards-list").content;
+
+// new place form popup
+const popupNewPlaceForm = document.querySelector('.page__popup_new-place');
+const nameInputNewPlaceForm = popupNewPlaceForm.querySelector('.popup__text-field_type_name');
+const jobInputNewPlaceForm = popupNewPlaceForm.querySelector('.popup__text-field_type_description');
+const saveButtonNewPlaceForm = popupNewPlaceForm.querySelector('.popup__btn_action_save');
+const closeButtonNewPlaceForm = popupNewPlaceForm.querySelector('.popup__btn_action_close');
+// new place events
+popupNewPlaceForm.addEventListener('submit', addNewCard);
+closeButtonNewPlaceForm.addEventListener('click', function() {togglePopupHandler(popupNewPlaceForm)});
+
+// place image popup
+const popupImage = document.querySelector('.page__popup_card-image');
+const closeButtonImage = popupImage.querySelector('.popup__btn_action_close');
+// place image events
+closeButtonImage.addEventListener('click', function() {togglePopupHandler(popupImage)});
+
 const cardItemTemplate = document.querySelector("#card-item").content;
-const popupTemplate = document.querySelector("#popup").content;
-const popupCardTemplate = document.querySelector("#popupCard").content;
-
-const cardsSection = document.querySelector('.elements');
-const popupSection = document.querySelector('.popup');
-
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+const cardsList = document.querySelector('.elements__list');
 
 function initCardsGrid() {
-
-    const cardsList = cardsListTemplate.querySelector(".elements__list").cloneNode(true);
     initialCards.forEach(function(item) {
-        let newCard = makeCard(item['name'], item['link']);
+        const newCard = makeCard(item['name'], item['link']);
         cardsList.appendChild(newCard);
     })
-    cardsSection.append(cardsList);
-}
-
-function initPopupForm(header, firstField, secondField, buttonText) {
-    popupForm = popupTemplate.cloneNode(true);
-    popupForm.querySelector('.popup__header').textContent = header;
-    
-    nameInput = popupForm.querySelector('.popup__text-field_type_name')
-    nameInput.value = firstField;
-    
-    jobInput = popupForm.querySelector('.popup__text-field_type_description')
-    jobInput.value = secondField;
-
-    formSaveButton = popupForm.querySelector('.popup__btn_action_save')
-    formSaveButton.textContent = buttonText;
-
-    formCloseButton = popupForm.querySelector('.popup__btn_action_close')
-    formCloseButton.addEventListener('click', toggleFormHandler);
-    
-    popupSection.textContent = "";
-    popupSection.append(popupForm);
-}
-
-function initPopupCard (imageLink, caption) {
-    popupCard = popupCardTemplate.cloneNode(true);
-    formCloseButton = popupCard.querySelector('.popup__btn_action_close')
-    formCloseButton.addEventListener('click', toggleFormHandler);
-    
-    popupCard.querySelector('.popup__image').src = imageLink;
-    popupCard.querySelector('.popup__caption').textContent = caption;
-
-    popupSection.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
-    popupSection.textContent = "";
-    popupSection.append(popupCard);
 }
 
 function showEditProfile() {
-    initPopupForm("Редактировать профиль", ProfileName.textContent, ProfileDesc.textContent, "Сохранить");
-    popupSection.querySelector('form').addEventListener('submit', formSubmitHandler);
-    toggleFormHandler();
+    nameInputEditForm.value =  profileName.textContent;
+    jobInputEditForm.value = profileDesc.textContent;
+    togglePopupHandler(popupEditForm);
 }
 
 function showAddCard() {
-    initPopupForm("Новое место", "", "", "Создать");
-    nameInput.placeholder = "Название"
-    jobInput.placeholder = "Ссылка на картинку"
-    popupSection.querySelector('form').addEventListener('submit', addNewCard);
-    toggleFormHandler();
+    popupNewPlaceForm.addEventListener('submit', addNewCard);
+    togglePopupHandler(popupNewPlaceForm);
 }
 
 function showCard(evt) {
     const targetCard = evt.target
-    let link = targetCard.style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    let caption = targetCard.parentElement.querySelector('.card__text').textContent;
-    initPopupCard(link, caption);
-    toggleFormHandler();
+    const imageLink = targetCard.style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    const caption = targetCard.parentElement.querySelector('.card__text').textContent;
+    
+    popupImage.querySelector('.popup__image').src = imageLink;
+    popupImage.querySelector('.popup__caption').textContent = caption;
+    togglePopupHandler(popupImage);
 }
 
 function makeCard(name, link) {
@@ -119,8 +72,8 @@ function makeCard(name, link) {
     return cardItem
 }
 
-function toggleFormHandler() {
-    popupSection.classList.toggle('popup_opened');
+function togglePopupHandler(popup) {
+    popup.classList.toggle('popup_opened');
 }
 
 function setLike(evt) {
@@ -130,25 +83,23 @@ function setLike(evt) {
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    ProfileName.textContent = nameInput.value;
-    ProfileDesc.textContent = jobInput.value;
-    toggleFormHandler();
+    profileName.textContent = nameInputEditForm.value;
+    profileDesc.textContent = jobInputEditForm.value;
+    togglePopupHandler(popupEditForm);
 }
 
 function addNewCard(evt) {
     evt.preventDefault();
-    let name = nameInput.value;
-    let link = jobInput.value;
-    let cardsList = document.querySelector(".elements__list");
-    let newCard = makeCard(name, link);
+    const name = nameInputNewPlaceForm.value;
+    const link = jobInputNewPlaceForm.value;
+    const newCard = makeCard(name, link);
     cardsList.prepend(newCard);
-    toggleFormHandler();
+    togglePopupHandler(popupNewPlaceForm);
 }
 
 function deleteCard(evt) {
     evt.stopPropagation();
     evt.target.closest('.elements__item').remove();
-    console.log(cardsSection);
 }
 
 initCardsGrid();
